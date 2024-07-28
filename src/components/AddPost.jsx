0a3746d9f889024bde6b1e7acff74cc5afbea996
Diyarose <dhiyarosedavis@gmail.com>
2024-07-28 20:40:09 +0530

    viewall completed

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import NavBar from './NavBar'
 
 const AddPost = () => {
   
@@ -39,6 +40,7 @@ const AddPost = () => {
 
   return (
     <div>
+        <NavBar/>
         <div className="container">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                 <div className="row g-3">
